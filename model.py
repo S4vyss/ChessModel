747b17d333e57f1b1055d36e51f51328c5b65f2c
S4vyss <92021800+S4vyss@users.noreply.github.com>
2023-04-16 12:25:21 +0200

    First prototype of ChessModel created.

--- model.py
+++ model.py
@@ -1,8 +1,14 @@
 import re
 
+import torch
+
 from evaluation import evaluation
 import chess.pgn
 import numpy as np
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+import torch.nn.functional as F
+import os
 
 # board format
 # r n b q k b n r
@@ -18,6 +24,8 @@ data = open('fide2000Games.pgn')
 data = data.read()
 data = data.split('\n\n')
 data = [x for x in data if not x.startswith('[')]
+
+game = chess.Board()
 
 for i in range(len(data)):
     data[i] = re.sub('{.*}', '', data[i])
@@ -84,4 +92,124 @@ def move_rep(move, board):
     return np.stack([from_output_layer, to_output_layer])
 
 def create_move_list(s):
-    return re.sub('\d*\. ', '', s).split(' ')[:-1]
+    moves = re.sub('\d*\. ', '', s).split(' ')[:-1]
+    return list(filter(lambda x: x != '', moves))
+
+class ChessDataset(Dataset):
+    def __init__(self, games):
+        super(ChessDataset, self).__init__()
+        self.games = np.array(games)
+
+    def __len__(self):
+        return 2000
+
+    def __getitem__(self, index):
+        while True:
+            game_i = np.random.randint(self.games.shape[0])
+            random_game = data[game_i]
+            moves = create_move_list(random_game)
+            if moves:
+                break
+
+        if len(moves) <= 1:
+            return self.__getitem__(index)
+        game_state_i = np.random.randint(len(moves) - 1)
+
+        next_move = moves[game_state_i]
+        moves = moves[:game_state_i]
+
+        board = chess.Board()
+
+        for move in moves:
+            board.push_san(move)
+
+        x = board_to_rep(board)
+        y = move_rep(next_move, board) # May want to try to use stockfish's next move as opposed to next move played in the game
+        if game_state_i % 2 == 1:
+            x *= 1
+        return x, y
+
+data_train = ChessDataset(data)
+data_train_loader = DataLoader(data_train, batch_size=32, shuffle=True, drop_last=True)
+
+class Module(nn.Module):
+
+    def __init__(self, hidden_size):
+        super(Module, self).__init__()
+        self.conv1 = nn.Conv2d(hidden_size, hidden_size, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(hidden_size, hidden_size, 3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(hidden_size)
+        self.bn2 = nn.BatchNorm2d(hidden_size)
+        self.activation1 = nn.SELU()
+        self.activation2 = nn.SELU()
+
+    def forward(self, x):
+        x_input = torch.clone(x)
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.activation1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = x + x_input
+        x = self.activation2(x)
+        return x
+
+class ChessNetwork(nn.Module):
+
+    def __init__(self, hidden_layers=4, hidden_size=200):
+        super(ChessNetwork, self).__init__()
+        self.hidden_layers = hidden_layers
+        self.input_layer = nn.Conv2d(6, hidden_size, 3, stride=1, padding=1)
+        self.module_list = nn.ModuleList([Module(hidden_size) for i in range(hidden_layers)])
+        self.output_layer = nn.Conv2d(hidden_size, 2, 3, stride=1, padding=1)
+
+    def forward(self, x):
+
+        x = x.float()
+        x = self.input_layer(x)
+        x = F.relu(x)
+
+        for i in range(self.hidden_layers):
+            x = self.module_list[i](x)
+
+        x = self.output_layer(x)
+
+        return x
+
+metric_from = nn.CrossEntropyLoss()
+metric_to = nn.CrossEntropyLoss()
+
+model = ChessNetwork()
+
+# Optimizer
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+for epoch in range(10):
+    epoch_loss = 0.0
+    for batch_index, (inputs, targets) in enumerate(data_train_loader):
+
+        optimizer.zero_grad()
+        outputs = model(inputs)
+
+        # Loss
+
+        loss_from = metric_from(outputs[:, 0, :], targets[:, 0, :])
+        loss_to = metric_to(outputs[:, 1, :], targets[:, 1, :])
+        loss = loss_from + loss_to
+
+        loss.backward()
+        optimizer.step()
+
+        # Accumulate the loss
+
+        epoch_loss += loss.item()
+
+    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, 10, epoch_loss / len(data_train_loader)))
+
+if not os.path.exists('trainedModel'):
+    os.mkdir('trainedModel')
+
+model_path = os.path.join('trainedModel', 'model.pt')
+torch.save(model.state_dict(), model_path)
