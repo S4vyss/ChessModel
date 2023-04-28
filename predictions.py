f84e6f1925bed79e90f944842d9610d16de82f2c
S4vyss <92021800+S4vyss@users.noreply.github.com>
2023-04-28 19:03:43 +0200

    Tried building eval function, a little more improvement needed

--- predictions.py
+++ predictions.py
@@ -14,6 +14,16 @@ letter_2_num = {
     'h': 7
 }
 
+piece_values = {
+    chess.PAWN: 1,
+    chess.KNIGHT: 3,
+    chess.BISHOP: 3,
+    chess.ROOK: 5,
+    chess.QUEEN: 9,
+    chess.KING: 0
+}
+
+
 model = ChessNetwork()
 model.load_state_dict(torch.load('trainedModel/model.pt'))
 model.eval()
@@ -44,11 +54,91 @@ def distribution_over_moves(vals):
 #         print(model(x))
 #     return output.argmax().item()
 
+def eval_board(board):
+    score = 0
+
+    # Add up the material score
+    for piece_type, value in piece_values.items():
+        score += value * len(board.pieces(piece_type, chess.WHITE))
+        score -= value * len(board.pieces(piece_type, chess.BLACK))
+
+    # Check if either h or a pawn has moved
+    has_h_pawn_moved = any(move.from_square == chess.H2 or move.to_square == chess.H2 for move in board.move_stack)
+    has_a_pawn_moved = any(move.from_square == chess.A2 or move.to_square == chess.A2 for move in board.move_stack)
+
+    # Penalize for starting with h or a pawns
+    if not has_h_pawn_moved:
+        score -= 0.2
+    if not has_a_pawn_moved:
+        score -= 0.2
+
+    # Penalize hanging pieces
+    for square, piece in board.piece_map().items():
+        if piece.color == board.turn:
+            attackers = [attacker for attacker in board.attackers(not piece.color, square)]
+            try:
+                attackers = [attacker for attacker in attackers
+                             if piece_values.get(attacker.piece_type, 0) >= piece_values.get(piece.piece_type, 0)]
+            except AttributeError:
+                print()
+
+            if not attackers and piece_values.get(piece.piece_type, 0) > 1:
+                score -= piece_values.get(piece.piece_type, 0) / 2
+
+    # Add a bonus for controlling the center
+    center_squares = [chess.D4, chess.D5, chess.E4, chess.E5]
+    king_square = board.king(board.turn)
+
+    for square in center_squares:
+        piece = board.piece_at(square)
+        if piece is not None and piece.color == board.turn:
+            score += 0.4 * piece_values.get(piece.piece_type, 0)
+
+    # Add a bonus for having a more active position
+    for piece in board.piece_map().values():
+        if isinstance(piece, chess.Piece) and piece.color == board.turn:
+            try:
+                mobility = len(board.attacks(piece.square))
+                score += 0.5 * piece_values.get(piece.piece_type, 0) * mobility
+            except AttributeError:
+                print()
+    for square in center_squares:
+        if board.is_controlled(square) and chess.square_distance(square, king_square) <= 4:
+            score += 0.1
+
+        # Add a bonus for piece mobility near the king
+    for piece_type in [chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN]:
+        for piece in board.pieces(piece_type, board.turn):
+            if chess.square_distance(piece, king_square) <= 4:
+                mobility = len(board.attacks(piece))
+                score += 0.2 * piece_values.get(piece_type, 0) * mobility
+
+    print("Score: {}".format(score))
+
+    return score
+
 def choose_move(board, player, color):
     legal_moves = list(board.legal_moves)
     best_move = None
     best_score = float('-inf') if player == chess.WHITE else float('inf')
     for move in legal_moves:
+        # check if move leads to hanging piece
+        if board.is_capture(move):
+            piece_captured = board.piece_at(move.to_square)
+            if piece_captured is not None:
+                eval_board_after_move = eval_board(board)
+                board.push(move)
+                if board.is_check():
+                    eval_board_after_move -= 1.0 * piece_values[piece_captured.piece_type]
+                if eval_board_after_move < eval_board(board):
+                    board.pop()
+                    continue
+                # check if capturing piece has equal or lower value compared to the captured piece
+                piece_capturing = board.piece_at(move.from_square)
+                if piece_capturing is not None and piece_values.get(piece_capturing.piece_type, 0) <= piece_values.get(piece_captured.piece_type, 0):
+                    best_score += 0.5 * piece_values[piece_captured.piece_type]  # add a fixed bonus to the score
+                board.pop()
+
         x = torch.Tensor(board_to_rep(board)).float()
         if color == chess.BLACK:
             x *= -1
@@ -64,8 +154,6 @@ def choose_move(board, player, color):
             best_move = move
             best_score = score
     return best_move
-
-
 
 
 gameBoard = chess.Board()
